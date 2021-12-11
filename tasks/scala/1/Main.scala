7c0574c8d03f728d208c5b688e07f7ecde0ac6d1
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2021-12-11 22:39:46 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,33 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == r) || (c == 0))
+      1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def countBrackets(chars: List[Char], openBrackets: Int): Boolean = {
+
+      if (chars.isEmpty)
+        openBrackets == 0
+      else {
+        val num = 0
+        if (chars.head == '(')
+          openBrackets + 1
+        else if (chars.head == ')')
+          openBrackets - 1
+        else
+          openBrackets
+        if (num >= 0)
+          countBrackets(chars.tail, num)
+        else false
+      }
+    }
+    countBrackets(chars, 0)
   }
 
   /**
@@ -33,6 +52,10 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)
+      1
+    if ((money < 0) || (coins.isEmpty && money > 0))
+      0
+    countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
