19f43653da429341c2fb7f668b9fde4607923dc9
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-03-18 16:20:27 +0300

    Update 3_quicksort.pl

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,40 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+qsort([X|XTail], XSorted):-
+ partition(X, XTail, L, R),
+    qsort(L, LSorted),
+    qsort(R, RSorted),
+ append(LSorted, [X|RSorted], XSorted).
+
+qsort([],[]).
+
+
+partition(X, [CompElem|XTail], [CompElem|L], R):-
+    CompElem =< X,
+    partition(X, XTail, L, R).
+
+partition(X,[CompElem|XTail],L,[CompElem|R]):-
+    CompElem > X,
+    partition(X, XTail, L, R).
+
+partition(X,[],[],[]).
+
+
+split_middle(X, L, R):-
+ length(X, Len),
+ M is div(Len, 2),
+ length(L, M),
+ append(L, R, X).
+
+
+build_balanced_tree(List, instant(Root, LTree, RTree)) :-
+ split_middle(List, L, [Root|R]),
+ balanced_tree(L, LTree),
+ balanced_tree(R, RTree).
+
+build_balanced_tree([], empty).
+
+
+balanced_tree(L, T) :-
+ qsort(L, LSorted),
+ build_balanced_tree(LSorted, T).
+%?- balanced_tree([3,2,4],instant(3, instant(2, empty, empty), instant(4, empty, empty)))
+%true
